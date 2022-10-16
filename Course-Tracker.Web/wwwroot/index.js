35e075a54e3171abc5b48be5422aef20023f53b7
Ayo Ogunsanya <aaolamiji@gmail.com>
2022-10-16 17:08:11 -0400

    update gitignore

--- Course-Tracker.Web/wwwroot/index.js
+++
@@ -1,9 +0,0 @@
-// import * as React from 'react';
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-const root = ReactDOM.createRoot(document.getElementById('react-root'));
-root.render(React.createElement("div", { className: "container" },
-    React.createElement("header", null,
-        React.createElement("h1", null, "Ayo's Course Tracker")),
-    React.createElement("main", null,
-        React.createElement("p", null, "Welcome"))));
