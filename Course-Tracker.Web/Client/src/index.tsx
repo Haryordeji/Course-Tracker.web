d15910fc98f1f81be6bfab58c0d248a05dc53d59
Ayo Ogunsanya <aaolamiji@gmail.com>
2022-10-16 17:05:51 -0400

    set up .NET api + add typescript

---
+++ Course-Tracker.Web/Client/src/index.tsx
@@ -0,0 +1,14 @@
+// import * as React from 'react';
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('react-root'))!;
+root.render(
+    <div className = "container">
+    <header><h1>Ayo's Course Tracker</h1></header>
+    <main>
+        <p>Welcome</p>
+    </main>
+</div>
+);
+       
